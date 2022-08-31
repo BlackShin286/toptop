952eea303368cbfa12bbbc810cf5029ce50152fc
BlackShin286 <111301980+BlackShin286@users.noreply.github.com>
2022-08-31 14:45:20 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import requests, os, sys
+os.system("cls")
+while True : 
+	a = int(input("Nhập A : "))
+	while a<3000 : 
+		a=a+1
+		print("Anh Yêu Em "+str(a))
+	quit("Nhưng Mà Em Không Yêu Anh =))")
